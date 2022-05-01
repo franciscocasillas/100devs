91cf1b20ffc4a506e14528cc662b599b6cdb6fa1
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-01 13:04:38 -0500

    Tic Tac Toe first iteration

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,23 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+	constructor(brand, price, color, watts) {
+		this.brand = brand;
+		this.price = price;
+		this.color = color;
+		this.watts = watts;
+	}
+
+	noise() {
+		console.log("Room, room!");
+	}
+	turnOff() {
+		alert("This shit is off!");
+	}
+	turnAgain() {
+		alert("This shit is already on.");
+	}
+}
+
+let dolceGusto = new EspressoMachine("Nescafé", 5000, "black", 500);
+console.log(dolceGusto.turnOff());
---
+++ class30-materials/scratch/js/main.js
@@ -0,0 +1,65 @@
+//Set first player!
+let currentPlayer = "x";
+document.querySelector(".player").innerText = "Player's X turn";
+
+//Change player
+function changePlayer() {
+	if (currentPlayer === "x") {
+		currentPlayer = "o";
+		document.querySelector(".player").innerText = "Player's O turn";
+	} else {
+		currentPlayer = "x";
+		document.querySelector(".player").innerText = "Player's X turn";
+	}
+}
+
+// 3. Add event listener to every single square
+document.querySelector("#a1").addEventListener("click", () => {
+	gamePlay("#a1");
+});
+document.querySelector("#a2").addEventListener("click", () => {
+	gamePlay("#a2");
+});
+document.querySelector("#a3").addEventListener("click", () => {
+	gamePlay("#a3");
+});
+document.querySelector("#b1").addEventListener("click", () => {
+	gamePlay("#b1");
+});
+document.querySelector("#b2").addEventListener("click", () => {
+	gamePlay("#b2");
+});
+document.querySelector("#b3").addEventListener("click", () => {
+	gamePlay("#b3");
+});
+document.querySelector("#c1").addEventListener("click", () => {
+	gamePlay("#c1");
+});
+document.querySelector("#c2").addEventListener("click", () => {
+	gamePlay("#c2");
+});
+document.querySelector("#c3").addEventListener("click", () => {
+	gamePlay("#c3");
+});
+
+function gamePlay(square) {
+	if (currentPlayer === "x") {
+		document.querySelector(square).classList.add("exes");
+		document.querySelector(square).style.backgroundColor = "green";
+        checkWin()
+		changePlayer();
+	} else {
+		document.querySelector(square).classList.add("os");
+		document.querySelector(square).style.backgroundColor = "purple";
+        checkWin()
+		changePlayer();
+	}
+}
+t;
+// 5. Check out if the player won
+function checkWin() {
+	if (currentPlayer === "x") {
+        if ()
+	}
+}
+// 6. Reset everything
