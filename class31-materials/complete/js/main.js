52a93afa103ce7e85d61b78f6cc87f00a3c2c802
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-03 19:13:07 -0500

    Prototypes practice creating farm animals

--- class31-materials/complete/js/main.js
+++ class31-materials/complete/js/main.js
@@ -1,47 +1,63 @@
-class Animal{
-    constructor(name){
-        this._name = name
-    }
-    get name(){
-        return this._name
-    }
-    speak(){
-        console.log(`${this._name} makes a sound`)
-    }
+class Animal {
+	constructor(name) {
+		this._name = name;
+	}
+	get name() {
+		return this._name;
+	}
+	speak() {
+		console.log(`${this._name} makes a sound`);
+	}
 }
-class Dog extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
-}
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
+class Dog extends Animal {
+	constructor(name, breed) {
+		super(name);
+		this._breed = breed;
+	}
+	get breed() {
+		return this._breed;
+	}
+	speak() {
+		super.speak();
+		console.log(`${this.name} barks`);
+	}
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
+class Cat extends Animal {
+	constructor(name, breed) {
+		super(name);
+		this._breed = breed;
+	}
+	get breed() {
+		return this._breed;
+	}
+	speak() {
+		super.speak();
+		console.log(`${this.name} meows`);
+	}
+}
 
-let farm = [simba,machi,salem]
+class Horse extends Animal {
+	constructor(name, color) {
+		super(name);
+		this._color = color;
+	}
+	get color() {
+		return this._color;
+	}
+	speak() {
+		super.speak();
+		console.log(`${this.name} brams`);
+	}
+}
 
-for( a of farm ){
-    a.speak()
+let simba = new Dog("Simba", "Shepard");
+let machi = new Dog("The Machine", "Pitbull");
+let salem = new Cat("Salem", "American Shorthair");
+let carrot = new Horse("Carrot", "brown");
+
+let farm = [simba, machi, salem, carrot];
+
+for (a of farm) {
+	a.speak();
 }
