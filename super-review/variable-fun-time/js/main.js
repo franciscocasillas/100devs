af35966e71fa2f7a9902bbfcadad001791d2d45e
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-15 15:07:31 -0500

    Exercise 01 done

--- super-review/variable-fun-time/js/main.js
+++ super-review/variable-fun-time/js/main.js
@@ -1,18 +1,27 @@
 //--- Easy
 //create a variable and assign it a number
+let num = 30;
 
 //minus 10 from that number
+num -= 10;
 
 //print that number to the console
-
+console.log(num);
 //--- Medium
 //create a variable that holds a value from the input
 
 //add 25 to that number
+function funLess() {
+	const fun = Number(document.querySelector("#danceDanceRevolution").value);
+	let funLes = fun + 25;
+	alert(funLes);
+	console.log(fun + funLes);
+}
 
 //alert that number
 
 //--- Hard
 //create a variable that holds the h1
+document.querySelector("h1").addEventListener("click", funLess);
 
 //add an event listener to that element that console logs the sum of the two previous variables
