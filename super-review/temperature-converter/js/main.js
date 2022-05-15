87c33019cd5558f44024915182ecc7d2d2efb60b
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-15 16:22:33 -0500

    Exercise 04 done

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//1. Get temperature in Celciuls
+//2. Convert to Fahrenheit
+//3. Display result
+
+document.querySelector("button").addEventListener("click", toFahrenheit);
+
+function toFahrenheit() {
+	const celcius = Number(document.querySelector("#celcius").value);
+	let fahrenheit = (celcius + 32) / 9;
+	document.querySelector(".result").innerText = fahrenheit;
+}
