b871bd69e3a36c233a629caca6775144182b5aaa
Francisco J. Casillas <chiccasillas@gmail.com>
2022-05-15 15:14:29 -0500

    Exercise 02 done

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -6,6 +6,26 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
+document.querySelector("h1").addEventListener("click", checkAge);
+
+function checkAge() {
+	const age = document.querySelector("#danceDanceRevolution").value;
+	if (age < 16) {
+		document.querySelector("p").innerText = "You cannot drive!";
+	} else if (age < 18) {
+		document.querySelector("p").innerText =
+			" You can hate from outside the club cause you can't  even get in";
+	} else if (age < 21) {
+		document.querySelector("p").innerText = "You cannot drink, mate!";
+	} else if (age < 25) {
+		document.querySelector("p").innerText = "You cannot rent cars affordably";
+	} else if (age < 30) {
+		document.querySelector("p").innerText =
+			"You cannot rent fancy cars affordably";
+	} else {
+		document.querySelector("p").innerText = "There's nothing else left for you";
+	}
+}
 
 //--- Harder
 //On click of the h1
