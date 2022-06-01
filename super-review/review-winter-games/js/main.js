0524e62258ac429e905ae9b5e115ceecf5e4e8bf
Francisco Casillas <chiccasillas@gmail.com>
2022-06-01 15:29:55 -0500

    Update files

--- super-review/review-winter-games/js/main.js
+++ super-review/review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function even(arr) {
+	const even = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			even.push(arr[i]);
+		}
+	}
+	console.log(even);
+}
+
+even([1, 4, 3, 5, 8, 9]);
