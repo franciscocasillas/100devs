2740480c041290863cbff75e99b3130af5fae9cd
Francisco J. Casillas <chiccasillas@gmail.com>
2023-01-26 12:43:43 -0600

    Class weekend boring exercise solved class 12

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -1,13 +1,31 @@
-document.querySelector("#check").addEventListener("click", check);
+document.querySelector("#check").onclick = goodDay;
 
-function check() {
+function goodDay() {
+	getDay();
+}
+
+function getDay() {
 	const day = document.querySelector("#day").value.toLowerCase();
 
 	if (day === "monday" || day === "tuesday" || day === "wednesday") {
-		document.querySelector("#placeToSee").innerText = "You suck!";
-	} else if (day === "thursday" || day === "friday") {
-		document.querySelector("#placeToSee").innerText = "Almost there";
+		document.querySelector("#placeToSee").innerText = "Boring Day";
+	} else if (day === "thursday") {
+		document.querySelector("#placeToSee").innerText = "Almost There";
 	} else {
-		document.querySelector("#placeToSee").innerText = "Yayyy";
+		document.querySelector("#placeToSee").innerText = "Weekend, yay!";
 	}
 }
+
+// document.querySelector("#check").addEventListener("click", check);
+
+// function check() {
+// 	const day = document.querySelector("#day").value.toLowerCase();
+
+// 	if (day === "monday" || day === "tuesday" || day === "wednesday") {
+// 		document.querySelector("#placeToSee").innerText = "You suck!";
+// 	} else if (day === "thursday" || day === "friday") {
+// 		document.querySelector("#placeToSee").innerText = "Almost there";
+// 	} else {
+// 		document.querySelector("#placeToSee").innerText = "Yayyy";
+// 	}
+// }
