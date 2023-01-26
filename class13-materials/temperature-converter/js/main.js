9f051e37d5e59b6e82d3e957ba8ad61ddf909d00
Francisco J. Casillas <chiccasillas@gmail.com>
2023-01-26 17:00:12 -0600

    Temperature converter exercise solved

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+document.querySelector("#converter").addEventListener("click", convert);
+
+function convert() {
+	const fah = document.querySelector("#fahrenheit").value;
+
+	let cel = Number(fah) + 2;
+
+	document.querySelector("#displayCelcius").innerText = cel;
+}
