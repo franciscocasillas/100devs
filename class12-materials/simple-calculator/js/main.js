4a4d8c4f3df112d494b5f91b8739847aef856eb2
Francisco J. Casillas <chiccasillas@gmail.com>
2023-01-26 12:54:12 -0600

    Simple calculator exercise solved

--- class12-materials/simple-calculator/js/main.js
+++ class12-materials/simple-calculator/js/main.js
@@ -1,26 +1,32 @@
-let total = 0
+let total = 0;
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+document.querySelector("#pumpkin").addEventListener("click", makeZero);
+document.querySelector("#dominosPizza").addEventListener("click", jumanji);
+document.querySelector("#zebra").addEventListener("click", add9);
+document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2);
+document.querySelector("#somethingCool").addEventListener("click", minusFive);
 
 function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
+	total = 0;
+	document.querySelector("#placeToPutResult").innerText = total;
 }
 
 function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+	total = total + 3;
+	document.querySelector("#placeToPutResult").innerText = total;
 }
 
 function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+	total = total + 9;
+	document.querySelector("#placeToPutResult").innerHTML = total;
 }
 
 function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+	total = total - 2;
+	document.querySelector("#placeToPutResult").innerHTML = total;
+}
+
+function minusFive() {
+	total = total - 5;
+	document.querySelector("#placeToPutResult").innerText = total;
 }
