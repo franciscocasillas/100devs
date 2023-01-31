f36aad8d9788cec5661fb0d5c24dad422e3fad86
Francisco J. Casillas <chiccasillas@gmail.com>
2023-01-31 17:58:40 -0600

    Class 13 practice

--- class13-materials/bachelor-code/2/js/main.js
+++ class13-materials/bachelor-code/2/js/main.js
@@ -1,21 +1,21 @@
-const andi = document.querySelector("#andi");
-const claire = document.querySelector("#claire");
-const sharleen = document.querySelector("#sharleen");
-
 document.querySelector("#andiNext").addEventListener("click", andiNext);
 document.querySelector("#claireNext").addEventListener("click", claireNext);
 document.querySelector("#sharleenNext").addEventListener("click", sharleenNext);
 
+let andi = document.querySelector("#andi");
+let claire = document.querySelector("#claire");
+let sharleen = document.querySelector("#sharleen");
+
 function andiNext() {
+	andi.classList.toggle("hidden");
 	claire.classList.add("hidden");
 	sharleen.classList.add("hidden");
-	andi.classList.toggle("hidden");
 }
 
 function claireNext() {
-	sharleen.classList.add("hidden");
 	andi.classList.add("hidden");
 	claire.classList.toggle("hidden");
+	sharleen.classList.add("hidden");
 }
 
 function sharleenNext() {
