3cdb6a8d4d65da055ee952671cab3e054d7054c5
Francisco J. Casillas <chiccasillas@gmail.com>
2023-01-31 17:27:20 -0600

    Class 13 practice

--- class13-materials/bachelor-code/1/js/main.js
+++ class13-materials/bachelor-code/1/js/main.js
@@ -4,10 +4,3 @@ function hide() {
 	document.querySelector("#claire").style.display = "none";
 	document.querySelector("#sharleen").style.display = "none";
 }
-
-// document.querySelector("#finalRose").addEventListener("click", hide);
-
-// function hide() {
-// 	document.querySelector("#claire").style.display = "none";
-// 	document.querySelector("#sharleen").style.display = "none";
-// }
