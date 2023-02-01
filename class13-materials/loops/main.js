a8fd70040d10c70ac9d33787bee55b2ba606c8aa
Francisco J. Casillas <chiccasillas@gmail.com>
2023-02-01 16:00:39 -0600

    Parity exercise solved

--- class13-materials/loops/main.js
+++ class13-materials/loops/main.js
@@ -12,3 +12,13 @@ while (number <= 10) {
 for (let i = 1; i <= 10; i++) {
 	console.log(i);
 }
+
+//PARITY
+let i = prompt("Give me a number from 1 to 10");
+for (i; i <= 10; i++) {
+	if (i % 2 === 0) {
+		console.log(`${i} is even`);
+	} else {
+		console.log(`${i} is odd`);
+	}
+}
