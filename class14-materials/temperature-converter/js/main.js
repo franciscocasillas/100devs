9c6410d1f5039afacb8bbc95a9eac77bcad2fc97
Francisco J. Casillas <chiccasillas@gmail.com>
2023-02-09 15:13:45 -0600

    Temperature converter solved

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,10 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//Get temperature
+document.querySelector("#convert").addEventListener("click", converter);
+
+//Convert temprature
+function converter() {
+	let temp = document.querySelector("#temperature").value;
+	temp = temp * 2;
+	document.querySelector("#result").innerText = `${temp} Celcius`;
+}
