5ff099304cbb3d470a3dd9bcec4dd7d42f7b6c22
Francisco J. Casillas <chiccasillas@gmail.com>
2023-02-09 15:17:45 -0600

    Temperature converter fixed to two decimals

--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -5,6 +5,7 @@ document.querySelector("#convert").addEventListener("click", converter);
 //Convert temprature
 function converter() {
 	let temp = document.querySelector("#temperature").value;
-	temp = temp * 2;
+	temp = ((temp - 32) * 5) / 9;
+	temp = temp.toFixed(2);
 	document.querySelector("#result").innerText = `${temp} Celcius`;
 }
