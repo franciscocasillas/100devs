7dc604cb6b5156fb84a9ad0a7a30917bda98f266
Francisco J. Casillas <chiccasillas@gmail.com>
2023-02-09 17:11:06 -0600

    Math equation functions

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,20 @@
 //create a function that adds two numbers and alerts the sum
+function sum(num1, num2) {
+	alert(num1 + num2);
+}
 
+sum(1, 3);
 //create a function that multiplys three numbers and console logs the product
 
+function multiply(n1, n2, n3) {
+	console.log(n1 * n2 * n3);
+}
+
+multiply(4, 5, 3);
+
 //create a function that divides two numbers and returns the ???
+function remainder(one, two) {
+	return one % two;
+}
+
+remainder(20, 4);
