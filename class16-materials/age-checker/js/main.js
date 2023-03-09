9269678a9a70c8403e28a0211ba30a2a5062a05c
Francisco J. Casillas <chiccasillas@gmail.com>
2023-03-09 14:16:35 -0600

    Age Checker exercise solved

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -6,8 +6,29 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector("h1").addEventListener("click", ageCheck);
+
+function ageCheck() {
+	let age = document.querySelector("#danceDanceRevolution").value;
+	if (age < 16) {
+		document.querySelector("p").innerText = "You can not drive";
+	} else if (age < 18) {
+		document.querySelector("p").innerText =
+			"You can't hate from outside the club, because you can't even get in";
+	} else if (age < 21) {
+		document.querySelector("p").innerText = "You can not drink";
+	} else if (age < 25) {
+		document.querySelector("p").innerText = "You can not rent cars affordably";
+	} else if (age <= 30) {
+		document.querySelector("p").innerText =
+			"You can not rent fancy cars affordably";
+	} else {
+		document.querySelector("p").innerText =
+			"There is nothing left to look forward to";
+	}
+}
