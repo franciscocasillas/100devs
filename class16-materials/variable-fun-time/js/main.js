173ec3987f799332b9e4e4ad3c35c4cc58daf2b8
Francisco J. Casillas <chiccasillas@gmail.com>
2023-03-09 13:35:45 -0600

    Variable fun time exercise solved

--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,30 @@
 //--- Easy
 //create a variable and assign it a number
+let coolNumber = 33;
 
 //minus 10 from that number
+let minusTen = coolNumber - 10;
 
 //print that number to the console
+console.log(minusTen);
 
 //--- Medium
 //create a variable that holds a value from the input
+let valerie = document.querySelector("#danceDanceRevolution").value;
 
 //add 25 to that number
+let addTwentyFive = valerie + 25;
 
 //alert that number
+alert(addTwentyFive);
 
 //--- Hard
 //create a variable that holds the h1
+let header = document.querySelector("h1");
 
 //add an event listener to that element that console logs the sum of the two previous variables
+header.addEventListener("click", numbers);
+
+function numbers() {
+	alert(Number(minusTen) + Number(addTwentyFive));
+}
