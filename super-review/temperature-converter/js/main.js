29d04146f1822ea537d0168e5eebd3c4571254b5
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-08 16:25:04 -0600

    Temperature converter exercise solved

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,13 +1,14 @@
 //Write your pseduo code first!
 
-//1. Get temperature in Celciuls
-//2. Convert to Fahrenheit
-//3. Display result
+//1. Listen to click on button
+//2. Get value from input
+//3. Convert value to number
+//4. Convert number to Fahrenheit
+//5. Display Fahrenheit value inside H2
 
-document.querySelector("button").addEventListener("click", toFahrenheit);
-
-function toFahrenheit() {
-	const celcius = Number(document.querySelector("#celcius").value);
-	let fahrenheit = (celcius + 32) / 9;
-	document.querySelector(".result").innerText = fahrenheit;
+document.querySelector("button").addEventListener("click", convertTemp);
+function convertTemp() {
+	const celcius = document.querySelector("#celcius").value;
+	const fahrenheit = celcius * 1.8 + 32;
+	document.querySelector("h2").innerText = fahrenheit;
 }
