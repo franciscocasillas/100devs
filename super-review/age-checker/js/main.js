9038a327013a6990dae41e41820dc8e51146f92f
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-08 15:05:24 -0600

    Age checker exercise solved

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -6,28 +6,28 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
-document.querySelector("h1").addEventListener("click", checkAge);
-
-function checkAge() {
-	const age = document.querySelector("#danceDanceRevolution").value;
-	if (age < 16) {
-		document.querySelector("p").innerText = "You cannot drive!";
-	} else if (age < 18) {
-		document.querySelector("p").innerText =
-			" You can hate from outside the club cause you can't  even get in";
-	} else if (age < 21) {
-		document.querySelector("p").innerText = "You cannot drink, mate!";
-	} else if (age < 25) {
-		document.querySelector("p").innerText = "You cannot rent cars affordably";
-	} else if (age < 30) {
-		document.querySelector("p").innerText =
-			"You cannot rent fancy cars affordably";
-	} else {
-		document.querySelector("p").innerText = "There's nothing else left for you";
-	}
-}
-
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector("h1").addEventListener("click", ageChecker);
+
+function ageChecker() {
+	const age = document.querySelector("input").value;
+	let message;
+	if (age < 16) {
+		message = "You can not dirve";
+	} else if (age < 18) {
+		message = "You can hate from outside the club";
+	} else if (age < 21) {
+		message = "You can not drink";
+	} else if (age < 25) {
+		message = "You can not rent cars affordably";
+	} else if (age < 30) {
+		message = "You can not rent fancy cars affordably";
+	} else {
+		message = "There is nothing left to look forward too";
+	}
+	document.querySelector("p").innerText = message;
+}
