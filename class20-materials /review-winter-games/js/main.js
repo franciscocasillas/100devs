7e1b36356d7a7a2d117ab3554365b6a67ded3086
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-19 13:42:19 -0600

    Review winter games solved

--- class20-materials /review-winter-games/js/main.js
+++ class20-materials /review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function duplicateArray(arr) {
+	let evens = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 !== 0) {
+			evens.push(arr[i]);
+		}
+	}
+	alert(evens);
+}
+
+duplicateArray([2, 3, 5, 7, 8, 9, 10]);
