924b5e0bbd3a7a3ba9f1037e236c8f3d1b3dea23
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-19 13:01:41 -0600

    Review summer games solved

--- class20-materials /review-summer-games/js/main.js
+++ class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,7 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+
+function sum(arr) {
+	alert(arr.reduce((result, x) => result * x));
+}
+
+sum([4, 5, 3]);
