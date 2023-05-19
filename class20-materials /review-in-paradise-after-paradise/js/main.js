ded9b358d94e04aed26cf8bd46e8c614b365651f
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-19 12:51:02 -0600

    Review in paradise solved

--- class20-materials /review-in-paradise-after-paradise/js/main.js
+++ class20-materials /review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,13 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function hiBye(arr) {
+	if (arr[0] < arr[arr.length - 1]) {
+		alert("Hi");
+	} else if (arr[arr.length - 1] < arr[0]) {
+		alert("Bye");
+	} else {
+		alert("We close in an hour");
+	}
+}
+
+hiBye([3, 4, 2, 1, 0, 3]);
