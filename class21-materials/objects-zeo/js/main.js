82be7f50ffb4a6d0f7ef53b9744b58c3d9d0dc88
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-23 17:10:32 -0600

    Objects Zeo exercise solved

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {
+	color: "black",
+	brand: "Casio",
+	size: "small",
+	year: 2014,
+
+	start: function () {
+		console.log("Start rigth away");
+	},
+	stop: function () {
+		console.log("Stop right now, thank you very much");
+	},
+	reset: function () {
+		console.log("Reset your brain");
+	},
+};
