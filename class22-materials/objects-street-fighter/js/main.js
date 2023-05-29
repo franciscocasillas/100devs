3dcf8b537ecb13811497b3339053b7b15a57613d
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-29 12:54:16 -0600

    Object street fighter exercise solved

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,35 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeFighter(
+	fighterName,
+	fighterAge,
+	fighterType,
+	fighterYell,
+	fighterPower
+) {
+	this.name = fighterName;
+	this.age = fighterAge;
+	this.type = fighterType;
+	this.yell = function () {
+		console.log(fighterYell);
+	};
+	this.power = function () {
+		console.log(fighterPower);
+	};
+}
+
+let honda = new MakeFighter(
+	"Honda",
+	33,
+	"Heavy weight",
+	"You're dead",
+	"Confess before you die"
+);
+
+let chunLi = new MakeFighter(
+	"Chun Li",
+	27,
+	"Fast player",
+	"Yata",
+	"You won't survive this!"
+);
