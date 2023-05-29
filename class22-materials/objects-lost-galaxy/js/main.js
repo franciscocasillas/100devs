122e75926423cea5f30dc2311a2d78c837bcf5e2
Francisco J. Casillas <chiccasillas@gmail.com>
2023-05-29 12:33:06 -0600

    Objects lost galaxy exercise solved

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,13 @@
 //Create a mouse object that has four properties and three methods
+const mouse = {
+	color: "gray",
+	size: "small",
+	speed: "fast",
+	brand: "Dell",
+	click() {
+		return "clickiticlick";
+	},
+	dblclick() {
+		return "click once, click again";
+	},
+};
