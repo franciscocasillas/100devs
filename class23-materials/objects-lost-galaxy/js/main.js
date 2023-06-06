32d77a4945431cccdbde8703d35eac7402bebadf
Francisco J. Casillas <chiccasillas@gmail.com>
2023-06-05 18:42:36 -0600

    Lost Galaxy exercise solved

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,16 @@
 //Create a dog object that has four properties and three methods
+let dog = {
+	size: "big",
+	color: "golden",
+	breed: "colli",
+	name: "Bruno",
+	bark() {
+		console.log("Woof");
+	},
+	jump() {
+		console.log("That was high!");
+	},
+	fetch() {
+		console.log("Whoosh");
+	},
+};
