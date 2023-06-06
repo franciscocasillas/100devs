e2ff506a3133e023432de995faa4944e2590eaa9
Francisco J. Casillas <chiccasillas@gmail.com>
2023-06-05 19:08:37 -0600

    Constructor exercise solved

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,45 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeFighter(
+	name,
+	gender,
+	age,
+	specialPower,
+	specialKick,
+	specialFist,
+	phrase
+) {
+	(this.name = name),
+		(this.gender = gender),
+		(this.age = age),
+		(this.specialPower = specialPower),
+		(this.specialKick = function () {
+			console.log(specialKick);
+		}),
+		(this.specialFist = function () {
+			console.log(specialFist);
+		}),
+		(this.phrase = function () {
+			console.log(phrase);
+		});
+}
+
+const chunLi = new MakeFighter(
+	"Chun Li",
+	"female",
+	26,
+	"Power kicks",
+	"You won't survive this!",
+	"This is deadly",
+	"Love me not!"
+);
+
+const Honda = new MakeFighter(
+	"E. Honda",
+	"male",
+	32,
+	"Power hits",
+	"You are not my enemy",
+	"You have no chance",
+	"Have fun!"
+);
