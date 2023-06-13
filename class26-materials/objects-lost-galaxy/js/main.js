e3cb19627c4238e1ac8b892d0c609fa580b9f635
Francisco J. Casillas <chiccasillas@gmail.com>
2023-06-13 17:10:40 -0600

    Objects lost galaxy exercise solved

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {};
+
+pizza.cheese = "cheddar";
+pizza.size = "XL";
+pizza.ingredientes = ["pepperoni", "jalapenos"];
+pizza.crust = "flat";
+
+pizza.send = function () {
+	console.log("On its way!");
+};
+
+pizza.order = function () {
+	console.log("What would you like to have?");
+};
+
+pizza.fun = function () {
+	console.log("I have no idea");
+};
