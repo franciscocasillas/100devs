c4fb670be58136b94bc1bfedabb81ba95bc24bc8
Francisco J. Casillas <chiccasillas@gmail.com>
2023-06-13 17:35:17 -0600

    Objects constructor fun exercise solved

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,26 @@
 //Create a constructor with 4 properties and 3 methods
+function MakePizza(ingredients, cheese, size, crust, delivery, order, time) {
+	this.ingredients = ingredients;
+	this.cheese = cheese;
+	this.size = size;
+	this.crust = crust;
+	this.delivery = function () {
+		console.log(delivery);
+	};
+	this.order = function () {
+		console.log(order);
+	};
+	this.time = function () {
+		console.log(time);
+	};
+}
+
+let pepperonni = new MakePizza(
+	["peppers", "pineapples"],
+	"cheddar",
+	"small",
+	"thin",
+	"On its way!",
+	"Your order has been placed",
+	"It's gonna be there in less than half an hour"
+);
