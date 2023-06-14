a4215ee9095ec61bdaf90421b86fa7bd79258b3e
Francisco J. Casillas <chiccasillas@gmail.com>
2023-06-14 12:57:56 -0600

    API Cocktail app solved

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,1 +1,29 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+//https://www.thecocktaildb.com/api.php
+
+//input
+document.querySelector("button").addEventListener("click", getCocktail);
+
+function getCocktail() {
+	let cocktail = document.querySelector("input").value;
+	console.log(cocktail);
+
+	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data.drinks[0]);
+
+			//title
+			document.querySelector("h2").innerText = data.drinks[0].strDrink;
+
+			//instructions
+			document.querySelector(".instructions").innerText =
+				data.drinks[0].strInstructions;
+
+			//thumbnail
+			document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
+}
