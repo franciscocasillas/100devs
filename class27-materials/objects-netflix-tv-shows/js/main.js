9625c1bf8035ca25818322826fc64f91afad5205
Francisco J. Casillas <chiccasillas@gmail.com>
2023-09-25 14:38:29 -0600

    Create a class exercise solved

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,18 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class MakeShow {
+	constructor(region, genre, language, seasons) {
+		this.region = region;
+		this.genre = genre;
+		this.language = language;
+		this.seasons = seasons;
+	}
+	play() {
+		alert("Let's start the show!");
+	}
+	stop() {
+		alert("See you soon");
+	}
+}
 
+let gossipGirl = new MakeShow("USA", "teen drama", "english", 10);
