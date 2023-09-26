8d417716b47623829b0c86a827e7c270c1d18968
Francisco J. Casillas <chiccasillas@gmail.com>
2023-09-26 16:45:27 -0600

    Local storage exercise solved

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,16 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
 
+if (!localStorage.getItem("counter")) {
+	localStorage.setItem("counter", 0);
+}
 
+document.querySelector("button").addEventListener("click", addOne);
+
+function addOne() {
+	let sum = Number(localStorage.getItem("counter"));
+	sum += 1;
+	localStorage.setItem("counter", sum);
+	document.querySelector("h2").innerText = sum;
+}
+
+localStorage.removeItem("count");
