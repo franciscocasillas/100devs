279d0540995d52db0f223c9e8db0672ded933939
Francisco J. Casillas <chiccasillas@gmail.com>
2023-09-26 13:59:51 -0600

    NASA API exercise solved

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -1,17 +1,27 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+	const choice = document.querySelector("input").value;
+	const url = `https://api.nasa.gov/planetary/apod?api_key=6HsGHlzG87cIYac5QVrGDuFkk5HHzkWgX77GbKzh&date=${choice}`;
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	fetch(url)
+		.then((res) => res.json()) // parse response as JSON
+		.then((data) => {
+			console.log(data);
+			if (data.media_type === "image") {
+				document.querySelector("img").style.display = "block";
+				document.querySelector("iframe").style.display = "none";
+				document.querySelector("img").src = data.hdurl;
+				document.querySelector("h3").innerText = data.explanation;
+			} else if (data.media_type === "video") {
+				document.querySelector("img").style.display = "none";
+				document.querySelector("iframe").style.display = "block";
+				document.querySelector("iframe").src = data.url;
+				document.querySelector("h3").innerText = data.explanation;
+			}
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
 }
-
