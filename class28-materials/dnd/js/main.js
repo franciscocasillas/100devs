d0585e551668f4fed7a9ddeed9b2606e89e01faa
Francisco J. Casillas <chiccasillas@gmail.com>
2023-10-02 14:07:44 -0600

    Correct API fetch and ul list items

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -5,21 +5,16 @@ function getFetch() {
 	const choice = document.querySelector("input").value;
 	const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
-	fetch("https://www.dnd5eapi.co/api/subclasses")
+	fetch(url)
 		.then((res) => res.json())
 		.then((data) => {
-			console.log(data);
-			const subclasses = [];
-			for (let i = 0; i < data.count; i++) {
-				subclasses.push(data.results[i].name);
-			}
-			console.log(subclasses);
-			const ul = document.querySelector("ul");
-			subclasses.forEach((item) => {
+			console.log(data.subclasses.length);
+			for (let i = 0; i < data.subclasses.length; i++) {
+				const ul = document.querySelector("ul");
 				const li = document.createElement("li");
-				li.textContent = item;
+				li.textContent = data.subclasses[i].name;
 				ul.appendChild(li);
-			});
+			}
 		})
 		.catch((err) => {
 			console.log(`error ${err}`);
