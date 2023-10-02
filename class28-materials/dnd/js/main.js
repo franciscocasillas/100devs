38d88ac3e7a4726b9eaf00b1b93fbc5f661640bd
Francisco J. Casillas <chiccasillas@gmail.com>
2023-10-02 13:43:19 -0600

    Get info from DnD API and append inside unordered list

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,17 +1,27 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+	const choice = document.querySelector("input").value;
+	const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+	fetch("https://www.dnd5eapi.co/api/subclasses")
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			const subclasses = [];
+			for (let i = 0; i < data.count; i++) {
+				subclasses.push(data.results[i].name);
+			}
+			console.log(subclasses);
+			const ul = document.querySelector("ul");
+			subclasses.forEach((item) => {
+				const li = document.createElement("li");
+				li.textContent = item;
+				ul.appendChild(li);
+			});
+		})
+		.catch((err) => {
+			console.log(`error ${err}`);
+		});
 }
-
