11d00c62e5fc51a1fac3ead49f1efbc589c3794d
Francisco J. Casillas <chiccasillas@gmail.com>
2023-11-06 16:36:23 -0600

    Create a constructor to build street fighter characters

--- super-review/objects-street-fighter/js/main.js
+++ super-review/objects-street-fighter/js/main.js
@@ -1,1 +1,24 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function CreateCharacter(name, age, gender, power, scream, catchPhrase) {
+	this.name = name;
+	this.age = age;
+	this.gender = gender;
+	this.power = power;
+
+	this.scream = function () {
+		console.log(scream);
+	};
+	this.catchPhrase = function () {
+		console.log(catchPhrase);
+	};
+}
+
+let eHonda = new CreateCharacter(
+	"Honda",
+	35,
+	"male",
+	"leg kick",
+	"yata",
+	"Whatever, dude!"
+);
