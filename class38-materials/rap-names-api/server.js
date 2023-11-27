de4d011f52ff373e629dcfcb83d4e11845615973
Francisco J. Casillas <chiccasillas@gmail.com>
2023-11-27 15:53:26 -0600

    Add gitignore file

--- class38-materials/rap-names-api/server.js
+++ class38-materials/rap-names-api/server.js
@@ -2,10 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+const rappers = {
+	"21 savage": {
+		age: 29,
+		birthName: "Shéyaa Bin Abraham-Joseph",
+		birthLocation: "London, England",
+	},
+	"chance the rapper": {
+		age: 40,
+		brithName: "Chancelor Bennet",
+		birthLocation: "Chicago, Illinois",
+	},
+	"nikki minaj": {
+		age: 29,
+		brithName: "Onika Tanya Maraj-Petty",
+		birthLocation: "Sain James, Port of Spain, Trinidad and Tobago",
+	},
+};
+
 app.get("/", (request, response) => {
 	response.sendFile(__dirname + "/index.html");
 });
 
+app.get("/api/:rapperName", (request, response) => {
+	const rapperName = request.params.rapperName.toLowerCase();
+	if (rappers[rapperName]) {
+		response.json(rappers[rapperName]);
+	} else {
+		response.json(rappers["nikki minaj"]);
+	}
+	response.json(rappers);
+});
+
 app.listen(PORT, () => {
-	console.log(`Your PORT is running inside of ${PORT}`);
+	console.log(`Your PORT is running inside of port ${PORT}`);
 });
